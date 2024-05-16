1d811e204cac63d5d89d76e7f99d3fcf74eb6fca
Deshan <nawarathnadeshan@gmail.com>
2024-05-16 13:20:50 +0530

    add setting icon

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,10 +1,17 @@
 import { View, Text} from 'react-native'
-
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TopBar() {
   return (
-    <View className="items-center bg-slate-300">
-      <Text>TopBar</Text>
-    </View>
+    <View className='bg-slate-300 p-4 flex flex-col items-center'>
+  <View className="flex-row justify-between w-full">
+    <View><Ionicons name='settings' size={30}/></View>
+    <View><Text>Overall Balance</Text></View>
+    <View><Text>R</Text></View>
+  </View>
+  <View className='item-center mt-2'><Text>Rs 2300</Text></View>
+</View>
+
+    
   )
 }
---
+++ components/navigation/ProfileIcon.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ProfileIcon() {
+  return (
+    <View>
+      <Text>ProfileIcon</Text>
+    </View>
+  )
+}
